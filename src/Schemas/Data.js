c29c19b54f3cfa00e58f96993146ba87cfaee0d8
Eduardo Calle <49166547+eduardokalle@users.noreply.github.com>
2024-06-23 13:17:34 -0500

    add changes update

---
+++ src/Schemas/Data.js
@@ -0,0 +1,141 @@
+export const Data = {
+  profile: {
+    name: "Eduardo Calle",
+    ocupation: "FullStack Software Developer",
+    location: "Cucuta, Colombia",
+    email: "eduardokallek@gmail.com",
+    telephone: "+57 3124796067",
+    image: "images/yop.jpg",
+  },
+  aboutMe: {
+    label: "Perfil",
+    description:
+      "Programador de Software, Ingeniero de Sistemas, capacitado para desarrollar aplicaciones informáticas en diversas tecnologías y múltiples lenguajes de programación, aplicando metodologías ágiles de desarrollo, incluyendo identificación de necesidades y diseños de sistemas basados en modelos de desarrollo.",
+  },
+  skills: {
+    technicalLabel: "Tecnologías",
+    softLabel: "Skills",
+    technicalSkills: [
+      "Angular",
+      "React",
+      "JavaScript",
+      "Typescript",
+      "Nodejs",
+      "React Native",
+      "Express",
+      "Ionic",
+      "NPM",
+      "Node.js",
+      "MongoDB",
+      "GraphQL",
+      "REST API",
+      "SQL",
+      "Git"
+    ],
+    softSkills: [
+      "Capacidad de análisis",
+      "Resolución de Problemas",
+      "Pensamiento Crítico",
+      "Trabajo en equipo",
+      "Comunicación",
+      "Proactividad",
+      "Creatividad",
+      "Autodidacta"
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Visitar el perfil de Eduardo Calle en LinkedIn",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/eduardo-calle-331410196",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Visitar el perfil de Eduardo Calle en GitHub",
+        name: "github",
+        url: "https://github.com/eduardokalle",
+        className: "bxl-github",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "MARZO. 2021 - Actualidad",
+        company: "GLUKY GROUP",
+        description: [
+          "Desarrollo y mejora continua en las tecnologías Node.js(JS/TS).",
+          "Desarrollo y mejora continua de PWA en las tecnologías Angular, React, Ionic y Cordova.",
+          "Manejo de versiones en Bitbucket, y GCP como infraestructura.",
+          "Firebase como tecnologia de hosting, despliegues"
+        ],
+      },
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "Oct. 2019 - - Actualidad",
+        company: "Freelance",
+        description: [
+          "Desarrollo de Web Services REST en Nodejs(JS/TS) acompañado de diferentes motores de BD como Mysql, MongoDB, SQL Server.",
+          "Desarrollo de Frontend y Mobile con tecnologías como Angular, React, React Native, Ionic y Cordova.",
+          "Manejo de versiones en github, y Azure como infraestructura.",
+        ],
+      },
+      {
+        title: "Analista de Aplicaciones",
+        period: "MAYO. 2018 - - MARZO. 2021",
+        company: "Cognox",
+        description: [
+          "Manejo de Base de datos NoSQL",
+          "Análisis de datos Y Soporte de apliaciones",
+          "Desarrollo de Frontend  con tecnologías como React y Vanilla js (Html , CSS , Javascript)",
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Ingeniería de Sistemas",
+        date: "2013- 2018",
+        institution: "UniRemington",
+      },
+    ],
+    proyects: [
+      {
+        name: "MVP",
+        company: "GLUKY GROUP",
+        period: "MARZO. 2021 - Actualidad",
+        description: [
+          "Mejoras en la aplicacion mobil y web  Angular e Ionic, para la fidelizacion de clientes y desarrollo de nuevos componentes.",
+        ],
+      },
+      {
+        name: "Admin de clientes",
+        company: "GLUKY GROUP",
+        period: "MARZO. 2021 - Actualidad",
+        description: [
+          "Mejoras en la aplicacion de administracion web  de clientes y desarrollo de nuevos componentes.",
+        ],
+      },
+      {
+        name: "Zuppi",
+        company: "Freelance",
+        period: "Oct. 2019 - - Actualidad",
+        description: [
+          "Desarrollo frontend y mobile con las tecnologias de Angular e Ionic Y React para el manejo de productos post y comandas.",
+          "Desarrollo NODE.JS, graphql , API REST para consulta y descarga datos.",
+        ],
+      },
+      {
+        name: "Puntos ya",
+        company: "Freelance",
+        period: "Oct. 2019 - - Actualidad",
+        description: [
+          "Desarrollo frontend y mobile con las tecnologias de Angular e Ionic Y React para el manejo de productos post y comandas.",
+          "Desarrollo NODE.JS, API REST para consulta y descarga datos.",
+        ],
+      },
+    ],
+  },
+};
